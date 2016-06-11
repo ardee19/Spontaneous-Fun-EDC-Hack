db8db52b277649d569ed843aae76424467d754e9
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2016-06-10 22:10:32 -0700

    Initial Commit

--- Spontaneous Fun/ViewController.swift
+++ Spontaneous Fun/ViewController.swift
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var tableViewContest: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
 

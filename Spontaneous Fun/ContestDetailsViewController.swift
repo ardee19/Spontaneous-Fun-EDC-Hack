18c9b3570c40380e24c5ef4fe2c9605dd190569f
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2016-06-11 23:49:47 -0700

    Add View for avatar and score

--- Spontaneous Fun/ContestDetailsViewController.swift
+++ Spontaneous Fun/ContestDetailsViewController.swift
@@ -15,9 +15,13 @@ class ContestDetailsViewController: UIViewController {
     @IBOutlet weak var contestTitle: UILabel!
     @IBOutlet weak var artist: UILabel!
     
+
+    @IBOutlet weak var scrollview: UIScrollView!
     
     var contest:Contest = Contest()
 
+    var totalScrollwidth = 600
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -28,6 +32,46 @@ class ContestDetailsViewController: UIViewController {
         contestTitle.text = contest.title
         artist.text = contest.artist
         
+        self.scrollview.contentSize = CGSizeMake(CGFloat(totalScrollwidth), 60)
+        
+        
+        let view: UIView = UIView(frame: CGRectMake(0, 0, 47, 60))
+//        view.backgroundColor = UIColor.greenColor()
+        let imageView = UIImageView(frame: CGRectMake(4, 4, 39, 39))
+        imageView.contentMode = .ScaleAspectFit
+        imageView.image = UIImage(named: "avatar-1")
+        
+        
+        imageView.layer.cornerRadius = imageView.frame.width / 2
+        imageView.layer.masksToBounds = true
+        
+        view.addSubview(imageView)
+        
+        let label:UILabel = UILabel(frame: CGRectMake(0, 45, 47, 14))
+        label.font = UIFont(name: "AvenirNext-Regular", size: 10)
+        label.textColor = UIColor.greenColor()
+        label.textAlignment = .Center
+        label.text = "^412"
+        view.addSubview(label)
+    
+        self.scrollview.addSubview(view)
+        
+//        for i in 1...10 {
+//            let iCG = CGFloat(integerLiteral: i)
+//            let view = UIView(frame: CGRectMake(60.0 * iCG,0,60,60))
+//            if i % 2 == 0 {
+//                view.backgroundColor = UIColor.purpleColor()
+//            } else {
+//                view.backgroundColor = UIColor.orangeColor()
+//            }
+//            self.scrollview.addSubview(view)
+//        }
+//        
+//        let view2: UIView = UIView(frame: CGRectMake(60, 0, 60, 60))
+//        view2.backgroundColor = UIColor.redColor()
+//        self.scrollview.addSubview(view2)
+
+
     }
 
     override func didReceiveMemoryWarning() {
